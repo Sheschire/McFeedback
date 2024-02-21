a4d1ab2bbf69be65d445604c148e9afd4ac0652f
TheoLmsl <62881349+Sheschire@users.noreply.github.com>
2024-02-21 18:52:24 +0100

    Update McFeedback.py

--- McFeedback.py
+++ McFeedback.py
@@ -1,12 +1,17 @@
+import sys
+import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import random
-from datetime import datetime
+from selenium.common.exceptions import TimeoutException
 import time
-import sys
+from datetime import datetime
 import schedule
+import random
+import sys
+import colorama
+from colorama import Fore, Back, Style
 
 # Return between 5 and 54
 def rand_5_55():
@@ -18,6 +23,7 @@ AGE = str(random.randrange(2, 6))
 ORDER_LOCATION = random.randrange(1, 4)
 EAT_LOCATION = random.randrange(1, 3)
 NOW = datetime.now()
+COUNTER = 0
 
 ##########################################################################################
 START_TIME_11h_15m = NOW.replace(hour=11, minute=random.randrange(15, 25), second=rand_5_55(), microsecond=rand_5_55())
@@ -26,12 +32,19 @@ END_TIME_23h_15m = NOW.replace(hour=23, minute=random.randrange(15, 25), second=
 
 
 def mc_feedback():
+    global COUNTER
+    COUNTER += 1
+    colorama.init()
+    print(Back.GREEN + Fore.LIGHTYELLOW_EX + f"McFeedback envoyés : {COUNTER}" + Style.RESET_ALL)
+
     date = str(time.strftime("%d/%m/%Y"))
     hour = NOW.strftime('%H')
     minute = NOW.strftime('%M')
 
     # Launch Chrome
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(options)
     driver.maximize_window()
     wait = WebDriverWait(driver, 10)
     driver.get("https://survey2.medallia.eu/?hellomcdo")
@@ -51,33 +64,33 @@ def mc_feedback():
     wait.until(EC.element_to_be_clickable((By.ID, "buttonNext"))).click()
 
     # Order Location
-    time.sleep(2)
+    time.sleep(4)
     wait.until(EC.presence_of_element_located((By.ID, "onf_q_where_did_you_place_your_order_"+str(ORDER_LOCATION)))).click()
-    time.sleep(1)
+    time.sleep(2)
     wait.until(EC.element_to_be_clickable((By.ID, "buttonNext"))).click()
 
     # In Shop
     if ORDER_LOCATION <= 2:
-        time.sleep(2)
+        time.sleep(4)
         wait.until(EC.presence_of_element_located((By.ID, "onf_q_feedback_m_where_did_you_eat_"+str(EAT_LOCATION)))).click()
-        time.sleep(1)
+        time.sleep(2)
         wait.until(EC.element_to_be_clickable((By.ID, "buttonNext"))).click()
 
         # Eat in
         if EAT_LOCATION == 1:
-            time.sleep(3)
+            time.sleep(4)
             wait.until(EC.presence_of_element_located((By.ID, "onf_q_where_was_the_order_delivered_1"))).click()
-            time.sleep(1)
+            time.sleep(2)
             wait.until(EC.element_to_be_clickable((By.ID, "buttonNext"))).click()
 
     # Satisfaction
-    time.sleep(2)
+    time.sleep(4)
     wait.until(EC.presence_of_element_located((By.ID, "onf_q_feedback_m_based_upon_this_visit_to_this_6_1"))).click()
-    time.sleep(1)
+    time.sleep(2)
     wait.until(EC.element_to_be_clickable((By.ID, "buttonNext"))).click()
 
     # More Satisfaction
-    time.sleep(2)
+    time.sleep(4)
     driver.find_element(By.ID, "onf_q_mc_q_friendliness_crew_1").click()
     driver.find_element(By.ID, "onf_q_mc_q_quality_of_food_and_drink_1").click()
     driver.find_element(By.ID, "onf_q_mc_q_speed_service_1").click()
@@ -85,37 +98,45 @@ def mc_feedback():
         driver.find_element(By.ID, "onf_q_feedback_m_the_exterior_aspect_of_the_res_1").click()
     else:
         driver.find_element(By.ID, "onf_q_mc_q_cleanliness_exterior_aspect_restaurant_1").click()
-    time.sleep(1)
+    time.sleep(2)
     wait.until(EC.element_to_be_clickable((By.ID, "buttonNext"))).click()
 
     # Command OK
-    time.sleep(2)
+    time.sleep(4)
     driver.find_element(By.ID, "onf_q_feedback_m_was_your_order_accurate_1").click()
-    time.sleep(1)
+    time.sleep(2)
     wait.until(EC.element_to_be_clickable((By.ID, "buttonNext"))).click()
 
     # Problem encounter
-    time.sleep(2)
+    time.sleep(4)
     driver.find_element(By.ID, "onf_q_feedback_m_did_you_experience_a_problem_d_2").click()
-    time.sleep(1)
+    time.sleep(2)
     wait.until(EC.element_to_be_clickable((By.ID, "buttonFinish"))).click()
     time.sleep(3)
 
 
 if __name__ == "__main__":
-    startMinutes = int(sys.argv[1])
-    endMinutes = int(sys.argv[2])
-
     # Schedule delay in minutes
-    schedule.every(startMinutes).to(endMinutes).minutes.do(mc_feedback)
+    delay = int(sys.argv[1])
+    if delay >= 10:
+        schedule.every(delay - 3).to(delay + 3).minutes.do(mc_feedback)
+    else:
+        schedule.every(delay).minutes.do(mc_feedback)
 
     # Wait for start time
     while NOW < START_TIME_11h_15m:
         NOW = datetime.now()
 
+    # Launch one at start
+    mc_feedback()
+
     while NOW < END_TIME_23h_15m:
         NOW = datetime.now()
-        schedule.run_pending()
+        try:
+            schedule.run_pending()
+        except TimeoutException:
+            print("Oops! Une erreur est survenue. Elle est rare, ne vous en faites pas. Le programme va continuer.")
+            pass
         time.sleep(1)
 
     exit()
